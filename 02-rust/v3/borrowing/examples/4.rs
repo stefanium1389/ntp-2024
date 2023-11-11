430b1cae2cba07f31bf3325d7ebbcc99cec63cde
n-dusan <nikolic.dusan.dey@gmail.com>
2023-11-11 19:06:35 +0100

    rust: v3

---
+++ 02-rust/v3/borrowing/examples/4.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let mut s = String::from("hello, ");
+
+    let p = __; // TODO
+    
+    p.push_str("world");
+
+    println!("Success!");
+}
---
+++ 02-rust/v3/borrowing/examples/5.rs
@@ -0,0 +1,11 @@
+// Ispravi grešku u kodu.
+fn main() {
+    let mut s = String::from("hello");
+
+    let r1 = &mut s;
+    let r2 = &mut s;
+
+    println!("{}, {}", r1, r2);
+
+    println!("Success!");
+}
